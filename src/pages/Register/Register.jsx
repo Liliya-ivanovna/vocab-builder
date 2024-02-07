b9dd838bccf269839dbee6b73fa72944b7ac2648
Liliya-ivanovna <lilyasviatenko@ukr.net>
2024-02-07 18:51:14 +0200

    Update Register.jsx

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -6,7 +6,7 @@ const Register=()=>{
     return (<>
    <div><svg width="40px" height="40px"> <use href={icons + '#icon-Craftwork'}/></svg>
    <h3>VocabBuilder</h3></div>
-   <img src={photo}/>
+   <img src={photo} alt="photoBG"/>
    <p>To start using our services, please fill out the registration form below. All fields are mandatory:</p>
    <h1>Register</h1>
     <RegisterForm/>
