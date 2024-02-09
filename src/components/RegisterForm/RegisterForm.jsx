846fb9cca54c11f8306c930aa8456cbed7383e91
Liliya-ivanovna <lilyasviatenko@ukr.net>
2024-02-09 19:50:58 +0200

    Update RegisterForm.jsx

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -3,42 +3,9 @@
 
 const RegisterForm=()=>{
     return(
-        <Formik>
-            <Form>
-            <label htmlFor="username">
-                <Field
-                  type="text"
-                  name="username"
-                  placeholder="Name"
-                  autoComplete="off"
-               
-                />
-                <ErrorMessage
-                  component="div"
-                  name="username"
-                 
-                />
-                
-              </label>
-              <label htmlFor="email">
-              <Field
-                type="text"
-                name="email"
-                placeholder="Email"
-                autoComplete="off"
-              />
-              </label>
-              <label htmlFor="password">
-              <Field
-                type='password'
-                name="password"
-                placeholder="Password"
-                autoComplete="off"
-                
-              />
-              </label>
-            </Form>
-        </Formik>
+        <>
+           
+        </>
     )
 }
 
